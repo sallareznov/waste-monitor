3e9c62e6f165f06963e5837a1ea9fd824f48deec
sallareznov <diagne14@gmail.com>
2016-04-19 02:50:24 +0200

    Encapsulates data for the signing up form

---
+++ app/models/UserData.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+  * Informations the user has to fill in order to sign up
+  * @param username the username provided by the user
+  * @param passwords the password and its confirmation, provided by the user
+  */
+case class UserData(username: String,  passwords: (String, String))
