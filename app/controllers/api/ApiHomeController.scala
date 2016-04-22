2b8743af9fd8a86df7fdaf15af07a0613d5f185b
Salla <salladiagne14@gmail.com>
2016-04-22 14:16:48 +0200

    conf/api.routes

--- app/controllers/api/ApiHomeController.scala
+++ app/controllers/api/ApiHomeController.scala
@@ -2,12 +2,12 @@ package controllers.api
 
 import javax.inject.Inject
 
-import models.{TokenRepository, UserRepository}
+import models.{ErrorJSONMessage, TokenRepository, UserRepository}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
-import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent.{ExecutionContext, Future}
 
 class ApiHomeController @Inject()(usersRepository: UserRepository, tokenRepository: TokenRepository, val messagesApi: MessagesApi)(implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
@@ -23,30 +23,32 @@ class ApiHomeController @Inject()(usersRepository: UserRepository, tokenReposito
     implicit request =>
       val tokens = credentials.split(",")
       val (username, password) = (tokens(0), tokens(1))
-      usersRepository.getByUsername(username).map({
+      usersRepository.getByUsername(username).flatMap {
         case None =>
-          /*usersRepository.add(username, password)
+          usersRepository.add(username, password)
           tokenRepository.addToken(username, password)
-          tokenRepository.getTokenForUsername(username) match {
-            case Future(Some(token)) => Ok(Json.toJson(token))
-            case _ => BadRequest
-          }*/
-          Ok
-        case Some(_) => Conflict
-      })
-
+          tokenRepository.getTokenForUsername(username).map({
+            case Some(token) => Ok(Json.toJson(token))
+            case _ => InternalServerError(Json.toJson(ErrorJSONMessage("Internal server error")))
+          })
+        case _ => Future.successful(Conflict(Json.toJson(ErrorJSONMessage("You are already registered"))))
+      }
   }
 
   def currentUser() = Action.async {
     implicit request => {
       request.headers.get("Authorization") match {
         case Some(authorizationHeader) =>
-          val tokenText = authorizationHeader.split(" ").last
-          tokenRepository.getToken(tokenText).map({
-            case Some(result) => Ok
-            case None => Unauthorized(Json.obj("message" -> "Bad credentials", "documentation" -> "https://github.com/sallareznov/waste-monitor"))
-          })
-        case None => Future.successful(Unauthorized(Json.obj("message" -> "Requires authentication", "documentation" -> "https://github.com/sallareznov/waste-monitor")))
+          val tokenTextOption = authorizationHeader.split(" ").lastOption
+          tokenTextOption match {
+            case None => Future.successful(BadRequest(Json.toJson(ErrorJSONMessage("Bad request"))))
+            case Some(tokenText) =>
+              tokenRepository.getToken(tokenText).map({
+                case Some(result) => Ok
+                case None => Unauthorized(Json.toJson(ErrorJSONMessage("Bad credentials")))
+              })
+          }
+        case None => Future.successful(Unauthorized(Json.toJson(ErrorJSONMessage("Requires authentication"))))
       }
     }
   }
